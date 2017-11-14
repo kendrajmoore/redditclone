f077d2ddbfa7794d89608c074902418d6f080600
Kendra Moore <kendra.j.moore3443@gmail.com>
2017-11-14 11:18:01 -0800

    ES6 routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,64 +5,73 @@ const User = require('../models/user.js');
 module.exports = (app) => {
 
   // SIGN UP
-    app.get('sign-up', (req, res) => {
-      res.render('sign-up').catch((err) => {
-        console.log(err);
-      })
-    })
-
+  app.get('sign-up', (req, res) => {
+    res.render('sign-up', {})
+  })
 
   //SIGN UP POST
-    app.post('/sign-up', function(req, res) {
-      // Create User and JWT
-      const user = new User(req.body);
-      console.log(user)
-  //AUTH USER TOKEN
-      user.save().then((user) => {
-        const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
-        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-        console.log(req.cookies)
-        res.redirect('/');
-      }).catch((err) => {
-        console.log(err.message);
-      });
+  app.post('/sign-up', function(req, res) {
+    const user = new User(req.body);
+    //AUTH USER TOKEN
+    user.save().then((user) => {
+      const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      console.log(req.cookies)
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err.message);
     });
+  });
 
 
-    //LOGIN FORM
+  //LOGIN FORM
+  app.get('/login', (req, res) => {
+    res.render('login', {})
+  })
 
-    app.get('/login', (req, res) => {
-      res.render('login').catch(err) => {
-        console.log(err);
+  // ...
+  app.post('/login', (req, res) => {
+    const password = req.body.password;
+    const username = req.body.username;
+    // ...
+    User.findOne({ username }).then((user) => {
+      if (!user) {
+        return res.status(401).send({ message: 'Wrong email or password' });
       }
-    })
-
-
-    app.post('/login', function(req, res) {
-      const password = req.body.password;
-      const username = req.body.username;
-      User.findOne({ username }.then((user) => {
-        if (!user) {
+      // ...
+      User.comparePassword(password, (err, isMatch) => {
+        // ...
+        if (!isMatch) {
           return res.status(401).send({ message: 'Wrong email or password' });
         }
-        user.comparePassword(req.body.password, function (err, isMatch) {
-          if (!isMatch) {
-            return res.status(401).send({ message: 'Wrong email or password' });
-          }
-          // creates req.user as  { _id: user._id }
-          const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { expiresIn: "60 days" });
-          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+        // ...
+        const token = jwt.sign({ _id: user._id, username: user.username },
+                                process.env.SECRET, { expiresIn: "60 days" });
 
-          res.redirect('/');
-        });
-      }).catch((err) => {
-        console.log(err);
-      });
-    });
-
-    app.get('/logout', (req, res)  => {
-      res.clearCookie('nToken').catch((err) => {
+        // ...
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
         res.redirect('/');
       })
-    })
-  }
+
+      // User.comparePassword(password, (err, isMatch) => {
+      //   if (!isMatch) {
+      //     return res.status(401).send({ message: 'Wrong email or password' });
+      //   }
+      //   // creates req.user as  { _id: user._id }
+      //   const token = jwt.sign({ _id: user._id, username: user.username },
+      //     process.env.SECRET, { expiresIn: "60 days" });
+      //   res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      //   res.redirect('/');
+
+    // ...
+    }).catch((err) => {
+         console.log(err);
+    });
+  })
+
+
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    res.redirect('/');
+  });
+}
--- controllers/comments.js
+++ controllers/comments.js
@@ -3,21 +3,20 @@ const Comment = require('../models/comment');
 
 module.exports = (app) => {
 
-
-
   // CREATE
   app.post('/posts/:postId/comments', (req, res) => {
     const currentUser = req.user;
     const postId = req.params.postId;
+    // ...
     Post.findById(postId).then((post) => {
       const comment = new Comment
     });
-        post.comments.unshift(comment);
-        post.save();
-
-        return res.redirect(`/posts/` + post._id);
-      }).catch((err) => {
+    post.comments.unshift(comment);
+    // ...
+    post.save().then((post) => {
+     res.redirect(`/posts/` + post._id);
+   }).catch((err) => {
         console.log(err);
-      });
-
+    });
+ });
 }
